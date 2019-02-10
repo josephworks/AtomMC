b478cb961391ebb33a5ebd1d677b47c7c5cb50be
Joseph Robinson <smartech56@gmail.com>
2019-02-10 18:50:59 +0000

    Removed Unused Imports in PlayerAdvancements.java and Block.java

--- src/main/java/net/minecraft/advancements/PlayerAdvancements.java
+++ src/main/java/net/minecraft/advancements/PlayerAdvancements.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
